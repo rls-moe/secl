f2f4cd820acb61b06d7029b315612820d919c12a
Tim Schuster <mail@timschuster.info>
2017-07-30 14:31:51 +0200

    Fixed up tests

--- exec/eval_test.go
+++ exec/eval_test.go
@@ -15,7 +15,7 @@ func TestEval(t *testing.T) {
 	ml, err := parser.ParseString(input)
 	assert.NoError(err)
 
-	assert.Equal(`( //MAP //LIST exec:( //MAP "hel"/STRING: exec:( //MAP //LIST nop/FUNCTION ) //LIST nop/FUNCTION exec:( //MAP //LIST nop/FUNCTION ) ) )`, types.PrintDebug(ml))
+	assert.Equal(`( //MAP //LIST exec:( //MAP "hel"/STRING(0:0): exec:( //MAP //LIST nop/FUNCTION ) //LIST nop/FUNCTION exec:( //MAP //LIST nop/FUNCTION ) ) )`, types.PrintDebug(ml))
 
 	mln, err := Eval(ml)
 	assert.NoError(err)
